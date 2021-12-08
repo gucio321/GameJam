a0de7d637c7527802fc771e592045503f82635e4
gucio321 <gucio321@protonmail.com>
2021-12-08 22:42:57 +0100

    letter: add ok button and complete writter script

--- Assets/Letter/LetterWritter.cs
+++ Assets/Letter/LetterWritter.cs
@@ -7,18 +7,24 @@ public class LetterWritter : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text textField;
+    [SerializeField, Tooltip("time (in seconds) between each print of letter")]
+    private float writterTimeDelta;
 
     private string text;
-
-    [ContextMenu("print")]
-    private void print()
-    {
-        WriteText("Hello world");
-    }
+    private float currentWritterTimeDelta;
+    private int currentLetter = 0;
+    private bool shouldWrite = false;
 
     public void WriteText(string t)
     {
-        textField.text = t;
+        text = t;
+        shouldWrite = true;
+    }
+
+    [ContextMenu("Clear")]
+    public void Clear()
+    {
+        textField.text = "";
     }
 
     // Start is called before the first frame update
@@ -30,6 +36,29 @@ public class LetterWritter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!shouldWrite)
+        {
+            return;
+        }
 
+        currentWritterTimeDelta += Time.deltaTime;
+
+        // check if there is a time to write next letter
+        if (currentWritterTimeDelta >= writterTimeDelta)
+        {
+            currentWritterTimeDelta = 0;
+            textField.text = textField.text + text[currentLetter];
+            currentLetter++;
+        }
+
+        // check if all the message was written
+        if (currentLetter == text.Length - 1)
+        {
+            // turn off everything
+            shouldWrite = false;
+            text = null;
+            currentLetter = 0;
+            currentWritterTimeDelta = 0;
+        }
     }
 }
