b6944951edb72d20f4d418da19b4618ec30bfc46
gucio321 <gucio321@protonmail.com>
2021-12-08 19:28:24 +0100

    WIP: add escape menu

---
+++ Assets/EscapeMenu/EscapeActions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeActions : MonoBehaviour
+{
+    public void OnExitClicked()
+    {
+        Debug.Log("Exit game - navigate to main menu");
+        // TODO: Load main menu scene here
+        // SceneManager.LoadScene("")
+    }
+}
---
+++ Assets/EscapeMenu/EscapeTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+
+    private void OnEscape()
+    {
+        // TODO: call game pause
+        // TODO: need to unlock mouse because it is hidden by PlayerController system
+        Cursor.lockState = CursorLockMode.None;
+
+        var active = canvas.activeSelf;
+        if (active)
+        {
+            OnMenuDisable();
+        }
+        else
+        {
+            OnMenuEnable();
+        }
+    }
+
+    private void OnMenuEnable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        canvas.SetActive(true);
+    }
+
+    private void OnMenuDisable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        canvas.SetActive(false);
+    }
+}
